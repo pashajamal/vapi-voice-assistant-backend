3c0a204ee338ea1996a920f79036f3428ca08c95
MOHAMMAD JAMAL <myjamal09@gmail.com>
2026-07-06 15:14:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -224,7 +224,7 @@ def delete_todo(request: VapiRequest, db: Session = Depends(get_db)):
     }
 
 
-# ============================================================
+# =====================================
 
 @app.post('/add_reminder/')
 def add_reminder(request: VapiRequest, db: Session = Depends(get_db)):
